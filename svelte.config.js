644b4e7a9a5da9be093745e5044cad95ff1a268c
kster735 <kster735@gmail.com>
2023-11-03 11:37:19 +0200

    Modified svelte.config.js for shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ const config = {
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  alias: {
+    $lib: "./src/lib"
+  },
 };
 export default config;
